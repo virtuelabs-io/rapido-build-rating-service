bca509a64c67171d315db635583290eb97e94f4e
Sangram Reddy <reddy.horcrux@gmail.com>
2019-09-16 21:24:04 +0100

    Added checkProductReview api

---
+++ src/ratings/checkProductReview.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const mysql = require('serverless-mysql')({
+    config: {
+        host: process.env.HOST,
+        user: process.env.USERNAME,
+        port: process.env.PORT,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
+    }
+})
+
+module.exports.fun = async (event, context, callback) => {
+    global.fetch = require('node-fetch');
+    console.log(event)
+    let customer_id = event.cognitoPoolClaims.sub
+    let product_id = Number(event.path.id)
+    let query = `
+        SELECT active
+            FROM ratings.feedback
+            WHERE customer_id = ?
+            AND   product_id = ?
+            AND active = TRUE;
+    `;
+    console.log("Running query", query);
+    let results = await mysql.query(query, [
+        customer_id,
+        product_id
+    ])
+    await mysql.end()
+    return results
+}
